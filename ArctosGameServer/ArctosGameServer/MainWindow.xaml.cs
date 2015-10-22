264f86b1267040ce751d83606e02df973f54293d
flomath <florian.mathis@students.fhv.at>
2015-10-22 17:12:08 +0200

    first tests with udp client/server for unity

---
+++ ArctosGameServer/ArctosGameServer/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ArctosGameServer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DoListen();
+        }
+
+        /// <summary>
+        /// Simple udp to unity test
+        /// </summary>
+        private void DoListen()
+        {
+            UdpClient udpServer = new UdpClient(11000);
+            var remoteEP = new IPEndPoint(IPAddress.Any, 11000);
+
+            while (true)
+            {
+                // player1;left;30%
+                var asdf = udpServer.Receive(ref remoteEP);
+                Console.Write("receive data from " + remoteEP.ToString());
+                //udpServer.Send(new byte[] { 1 }, 1, remoteEP); // reply back
+            }
+        }
+    }
+}
--- RoboArctosGame/Assets/Behaviours/ControllerInput.cs
+++ RoboArctosGame/Assets/Behaviours/ControllerInput.cs
@@ -1,20 +1,33 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
 
 public class ControllerInput : MonoBehaviour {
-
+	
 	// Use this for initialization
 	void Start () {
-	
+		print("startup");
+		var client = new UdpClient();
+		IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11000); // endpoint where server is listening
+		client.Connect(ep);
+		
+		client.Send(new byte[] { 1, 2, 3, 4, 5 }, 5);
+		temp = 0;
 	}
 	
+	float temp;
 	// Update is called once per frame
 	void Update () {
         float left = Input.GetAxis("TriggerLeft");
         float right = Input.GetAxis("TriggerRight");
 
         float speed = 10 * left - 10 * right;
-        print("SpeedLeft: " + speed);
-        GameObject.Find("Cube").transform.Translate(0, 0, speed);
+		if(temp != speed) {
+			temp = speed;
+			print("SpeedLeft: " + speed);
+			GameObject.Find("Cube").transform.Translate(0, 0, speed);
+		}
 	}
 }
