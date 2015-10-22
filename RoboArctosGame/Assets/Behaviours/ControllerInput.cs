0399baeb32ebb8ed841f857c2d48f17d3911b5aa
Philip Heimböck <philipheimboeck@gmail.com>
2015-10-22 14:28:32 +0200

    Unity project added

---
+++ RoboArctosGame/Assets/Behaviours/ControllerInput.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float left = Input.GetAxis("TriggerLeft");
+        float right = Input.GetAxis("TriggerRight");
+
+        float speed = 10 * left - 10 * right;
+        print("SpeedLeft: " + speed);
+        GameObject.Find("Cube").transform.Translate(0, 0, speed);
+	}
+}
