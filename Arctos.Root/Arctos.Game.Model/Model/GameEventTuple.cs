2cc62c3d5d2ea64b36d8eaae273eaa07b91c6421
flomath <florian.mathis@students.fhv.at>
2016-01-09 13:45:46 +0100

    added gameEventTuple

---
+++ Arctos.Root/Arctos.Game.Model/Model/GameEventTuple.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace Arctos.Game.Middleware.Logic.Model.Model
+{
+    [Serializable]
+    public class GameEventTuple
+    {
+        [XmlElement("string", Type = typeof(string))]
+        [XmlElement("bool", Type = typeof(bool))]
+        [XmlElement("int", Type = typeof(int))]
+        public object Item1 { get; set; }
+
+        [XmlElement("string", Type = typeof(string))]
+        [XmlElement("bool", Type = typeof(bool))]
+        [XmlElement("int", Type = typeof(int))]
+        public object Item2 { get; set; }
+    }
+}
